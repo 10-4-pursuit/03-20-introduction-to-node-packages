8e111723b9f79f10e8d5ed571a78fc3ad93542e3
GyancarlosPinto <gyancarlospinto@pursuit.org>
2023-12-18 20:58:17 -0500

    Completed Introduction to Node Packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        < ToastContainer/>
+        <button onClick={() => toast("Hello, this is a toast! CHEERS!")}>Show Toast</button>
+        <button onClick={() => toast("Simple Toast!")}>Show Simple Toast</button>
+        <button onClick={() => toast.success("Success message!")}>Success</button>
+        <button onClick={() => toast.error("ERROR MESSAGE!!!")}>Error</button>
+        <button onClick={() => toast.info("Info Message")}>Info</button>
+        <button onClick={() => toast.warning("THIS IS A WARNING MESSAGE!!!")}>Warning</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
