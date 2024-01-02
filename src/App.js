c9dfd33ad8ca554f4f6a48f772d3fda70670805e
clairep9 <clairepetitfrere@pursuit.org>
2024-01-01 21:38:07 -0500

    Completed Introduction to Node Packages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const toastFunction = () => toast("Hello, this is a small, non-intrusive message!", { 
+    position: "bottom-right",
+    style: {
+      backgroundColor: "blue",
+      color: "red",
+    },
+    autoClose: 10000,
+    closeButton: false,
+   });
+
+
+
+   const sucessToast = () => toast.success("Hello, this is a sucess message!", { 
+    position: "bottom-left",
+    style: {
+      backgroundColor: "gray",
+      color: "purple",
+    },
+    autoClose: 6000,
+    closeButton: true,
+   });
+
+
+
+   const errorToast = () => toast.error("Hello, this is an error message!", { 
+    position: "top-center",
+    style: {
+      backgroundColor: "gray",
+      color: "orange",
+    },
+    autoClose: 20000,
+    closeButton: true,
+   });
+
+
+   const infoToast = () => toast.info("Hello, this is an info message!", { 
+    position: "top-left",
+    style: {
+      backgroundColor: "salmon",
+      color: "green",
+    },
+    autoClose: 20000,
+    closeButton: false,
+   });
+
+
+   const warningToast = () => toast.warning("Hello, this is a warning message!", { 
+    position: "top-right",
+    style: {
+      backgroundColor: "yellow",
+      color: "red",
+    },
+    autoClose: 60000000,
+    closeButton: false,
+   });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={toastFunction}>Show Toast</button>
+      <button onClick={sucessToast}>Show Sucess</button>
+      <button onClick={errorToast}>Show Error</button>
+      <button onClick={infoToast}>Show Info</button>
+      <button onClick={warningToast}>Show Warning</button>
+      <ToastContainer />
     </div>
   );
 }
