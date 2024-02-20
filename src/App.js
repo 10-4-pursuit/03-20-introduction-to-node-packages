071f546ff8db3da931759fbdb68b82f80f0ac9cb
SaraM09 <sayeramusfika@pursuit.org>
2024-02-20 18:48:54 -0500

    Sara exercise 20

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Bounce, Flip, ToastContainer, toast} from 'react-toastify';
 
 function App() {
+  const success = () => toast.success('Wow so easy!');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+  <ToastContainer
+    position="bottom-left"
+    hideProgressBar={true}
+    theme="light"
+    transition = {Flip}
+    />
+    <button onClick={success}>  
+      Click me
+    </button>
+   
     </div>
+    
   );
 }
 
